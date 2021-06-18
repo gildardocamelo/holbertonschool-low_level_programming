70bca9d185d2110c1d76d15f7bf62d207e0e5cb1
root <root@lenovo-aio-510-A6.localdomain>
2021-06-18 12:41:09 -0500

    Low-level programming - variables: if-else-while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - numbers of base 10 starting from 0. (No char and only using putchar)
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+
+putchar((num % 10) + '0');
+
+putchar('\n');
+
+return (0);
+}
