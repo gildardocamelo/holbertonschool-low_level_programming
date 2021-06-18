565bcba5f8f6732966a1ee3bea6dae403087f18a
root <root@lenovo-aio-510-A6.localdomain>
2021-06-18 13:11:42 -0500

    Low-level programming - variables: if-else-while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int letter = 122;
+
+while (letter >= 97; letter ++)
+putchar(letter);
+
+putchar('\n');
+
+return (0);
+}
