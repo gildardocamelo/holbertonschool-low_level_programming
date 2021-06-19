bcc8e86d0c9f84a2bddbae99b37999ee13757f26
root <root@lenovo-aio-510-A6.localdomain>
2021-06-18 19:13:50 -0500

    Low-level programming - variables: if-else-while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase (only putchar and twice).
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	char low_alph = 97;
+
+	while (low_alph <= 122)
+	{
+		putchar(low_alph);
+		low_alph++
+	}
+
+	putchar('\n');
+
+	return (0);
+}
