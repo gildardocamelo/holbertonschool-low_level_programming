a5501c10e6ae6719e6ac1b163386516de2b831cf
root <root@lenovo-aio-510-A6.localdomain>
2021-06-21 23:46:17 -0500

    Low-level programming - functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add - adds two integers and returns the result.
+ *
+ * @x: The first number to be added.
+ *
+ * @y: The second number to be added.
+ *
+ * Return: result of the sum (x + y)
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
