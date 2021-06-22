2c877a7d91996a51c8a2381d0e812311fd0f90be
root <root@lenovo-aio-510-A6.localdomain>
2021-06-21 22:51:27 -0500

    Low-level programming - functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints the last digit of a number.
+ *
+ * Description: not allowed to include standard libraries, pintf and puts.
+ *
+ *@num: integer
+ *
+ * Return: value of the last digit.
+ */
+
+int print_last_digit(int num)
+{
+	if (n < 0)
+
+		n = n * -1;
+	_putchar((n % 10) + '0');
+
+	return (n % 10);
+}
