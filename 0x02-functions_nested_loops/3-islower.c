8c6602a87c82c9b47dc0828e85d0810c6965dd07
root <root@lenovo-aio-510-A6.localdomain>
2021-06-21 21:32:59 -0500

    Low-level programming - functions_nested_loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _islower - function that checks for lowercase character.
+ *
+ * Description: allowed twice _putchar and not allowed to include standard
+ * libraries, pintf and puts.
+ *
+ *@c: character
+ *
+ * Return: 0 for uppercase, 1 for lowercase
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c < 123)
+
+		return (1);
+
+	else
+
+		return (0);
+}
