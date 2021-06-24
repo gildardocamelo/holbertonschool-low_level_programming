def9bc8c44e7111309c86d57ad6beddc903bb932
root <root@lenovo-aio-510-A6.localdomain>
2021-06-24 18:54:47 -0500

    Low-level programming - More_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - function that multiplies two integers
+ *
+ * @a: number integer a
+ * @b: number integer b
+ *
+ * Return: result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
