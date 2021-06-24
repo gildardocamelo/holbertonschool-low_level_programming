0822d611397aef9f2ba5dd261ee6ed8cf28feedf
root <root@lenovo-aio-510-A6.localdomain>
2021-06-24 17:24:48 -0500

    Low-level programming - More_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * _isupper - function that checks for uppercase character
+ *
+ * @c: the character for checked
+ *
+ * Return: 1 if character is uppercase, 0 otherwise
+ */
+
+int _isupper(int c);
+{
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+	return (0);
+}
