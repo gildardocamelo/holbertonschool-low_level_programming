52df9cc8c941a5816400c674208734ec4fddbaeb
root <root@lenovo-aio-510-A6.localdomain>
2021-06-25 19:23:08 -0500

    Low-level programming - More_functions_nested_loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -17,13 +17,13 @@ void print_line(int n)
 
 	if (n <= 0)
 	{
-		putchar('\n');
+		_putchar('\n');
 	}
 	else if (n > 0)
 	{
 		for (i = 0 ; i < n ; i++)
 		{
-			putchar('_');
+			_putchar('_');
 		}
 		putchar('\n');
 	}
