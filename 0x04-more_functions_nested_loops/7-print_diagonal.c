877def5c76b72967a5a783b27062723a451298b4
root <root@lenovo-aio-510-A6.localdomain>
2021-06-25 22:43:40 -0500

    Low-level programming - More_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - function that draws a diagonal line on the terminal.
+ *
+ * Description: only use _putchar function to print, where n is the number of
+ * times the character \ should be printed, the line should end with a \n, if n
+ * is 0 or less, the function should only print \n
+ *
+ * @n: numbers line
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (j = 0 ; j <= n ; j++)
+	{
+		if (n <= 0)
+		{
+			putchar('\n');
+		}
+		else if (n > 0)
+		{
+			for (i = 0 ; i < n; i++)
+			{
+				if (i == j)
+
+					putchar('\\');
+
+				else if (i < j)
+
+					putchar (' ');
+
+			}
+			putchar('\n');
+		}
+	}
+}
