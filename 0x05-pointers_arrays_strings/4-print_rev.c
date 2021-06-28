f2872ed8b92e9e02a7e1048968e6666c593bf17a
root <root@lenovo-aio-510-A6.localdomain>
2021-06-28 16:23:48 -0500

    Low-level programming - pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_rev - function that prints a string, in reverse, followed by a
+ * new line.
+ *
+ * @s: pointer character.
+ *
+ */
+
+void print_rev(char *s)
+{
+        int n, m;
+
+	for (m = 0; s[m] != '\0'; m++)
+	{
+
+	}
+	for (n = m; n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}
