96b062d293e3cbbab6f6f28976b829f2c1f3066f
root <root@lenovo-aio-510-A6.localdomain>
2021-06-28 15:39:14 -0500

    Low-level programming - pointers_arrays_strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,7 +5,7 @@
  *
  * @s: pointer character.
  *
- * Return: length of a string.
+ * Return: length of string.
  */
 
 int _strlen(char *s)
@@ -13,6 +13,6 @@ int _strlen(char *s)
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
-		;
+		
 	return (i);
 }
