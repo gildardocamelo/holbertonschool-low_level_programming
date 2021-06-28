ae53133b59e2d09971d421ab6e6197f7ea0de3c8
root <root@lenovo-aio-510-A6.localdomain>
2021-06-28 15:35:56 -0500

    Low-level programming - pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout.
+ *
+ * @str: pointer character.
+ *
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
