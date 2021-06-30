b8f83b2ba1b2af3a3a63d20019a5044f85a90a04
root <root@lenovo-aio-510-A6.localdomain>
2021-06-29 21:39:21 -0500

    Low-level programming - pointers_arrays_strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,14 +8,27 @@
 
 void puts_half(char *str)
 {
-	int i;
+	int len, n, i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		if (i >= 5 && i != '\0')
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
 		{
-			putchar(str[i]);
+			_putchar(str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
 		}
 	}
-	putchar('\n');
+	_putchar('\n');
 }
